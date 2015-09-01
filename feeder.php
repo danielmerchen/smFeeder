2636c3ed32622385b8c89ba2341559f4a40c0f52
danielmerchen <dmerchen@danielmerchen.com>
2015-09-01 15:11:44 -0500

    Updated code to production code from early 2015.

--- feeder.php
+++ feeder.php
@@ -1,23 +1,34 @@
 <?php
-$datestring = strtotime('last Sunday');
-$week_start = date('Y-m-d', $datestring); 													// Start date is in YYYY-MM-DD format, this date will be relative to server time.
-$rebroadcast = array ("00:00","04:00","08:00","12:00","16:00","20:00"); 								// 24 Hour Schedule of Rebroadcast Times, Relative to Account Timezone
-$assets = array ("20140330_1_300k_0003.mp4", "20140330_1_600k_0003.mp4", "20140330_1_1200k_0003.mp4");	// Files to be Assoicated with Events, Does Not Check if they Actually Exist on Service
-$streamname = "simlive1";																				// Stream to push simulated live events to.
+// Apparently there is JSON of Available Files. Snag it later.
+$xml = new SimpleXMLElement('<xml/>');
+$xml->addAttribute('version', '1.0'); 
+$xml->addAttribute('encoding', 'UTF-8');
+
+$datestring = date('D');
+if($datestring == 'Sun'){
+	$datestring	= strtotime('this Sunday');	
+}
+else{
+	$datestring	= strtotime('last Sunday');	
+}
+//$datestring		= strtotime('last Sunday');
+$week_start 	= date('Y-m-d', $datestring); 										// Start date is in YYYY-MM-DD format, this date will be relative to server time.
+$rebroadcast 	= array ("00:00","03:59","07:59","11:59","15:59","19:59"); 			// 24 Hour Schedule of Rebroadcast Times, Relative to Account Timezone
+$assets 		= array ("filename.mp4");											// Files to be Assoicated with Events, Does Not Check if they Actually Exist on Service
+$streamname 	= "yourstreamname";													// Stream to push simulated live events to.
 
 $simlive_day = array(
-    "sunday"    => array ("13:15","15:00","17:30"), // Sunday exception to normal schedule.
-    "monday"  => $rebroadcast,
-    "tuesday"  => $rebroadcast,
+    "sunday"	=> array ("12:59", "14:59","16:59"), // Sunday exception to normal schedule.
+    "monday"	=> $rebroadcast,
+   	"tuesday"	=> $rebroadcast,
     "wednesday" => $rebroadcast,
     "thursday"  => $rebroadcast,
-    "friday" => $rebroadcast,
-    "saturday" => $rebroadcast,
+    "friday"	=> $rebroadcast,
+    "saturday"	=> $rebroadcast,
 );
 
-Header('Content-type: text/xml');
-Header('Content-Disposition: attachment; filename="'.date('Ymd', $datestring).'_schedule.xml"');
-
+header('Content-type: text/xml');
+header('Content-Disposition: attachment; filename="'.date('Ymd', $datestring).date('_YmdGis').'_schedule.xml"');
 echo '<?xml version="1.0" encoding="UTF-8" ?>'."\r\n"; ?>
 <schedule>
 	<!-- Start date is in YYYY-MM-DD format -->
@@ -34,7 +45,6 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>'."\r\n"; ?>
 							foreach ($assets as $files => $file) {						// Iterate through the array of files and create an file tag for each.
 							echo "				";
 							echo "<file>$file</file>\r\n";
-								//$file = $event->addChild('file', $file);
 							}
 						echo "			";
 						echo "</event>\r\n";
