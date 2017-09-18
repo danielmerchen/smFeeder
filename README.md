745f11e27e824210a8be09a9b02838dc59ca0be5
danielmerchen <dmerchen@danielmerchen.com>
2017-09-18 16:53:58 -0500

    Added README, Added latest revisions

---
+++ README.md
@@ -0,0 +1,17 @@
+# Stream Monkey Simulated Live Schedule Generator
+
+This small aids users in generating a repetative schedule of events using the Stream Monkey video streaming platform.
+
+## Getting Started
+
+Open the script, replace the variables for $asset and $rebroadcast to match your needs. Execute the script and upload the provided XML sheet to your account.
+
+### Environment and Usage
+
+This script has no other dependencies outside of PHP, it has been tested and working in 5.6.27 and is expected to function in future versions however they are untested.
+
+Simply access it via web server capable of executing PHP. Note the scripts use of headers to force a the download of an XML file for upload to the platform vendor.
+
+## Acknowledgments
+
+* Thanks to the Stream Monkey guys for providing a means of scheduling bulk events in the system.
--- feedgen.php
+++ feedgen.php
@@ -1,11 +1,11 @@
 <?php
-
-// Last Updated: 
-
-// Apparently there is JSON of Available Files. Snag it later.
+// Apparently there is JSON of available Files. Snag it later, will need to rebuilt the login system.
 $xml = new SimpleXMLElement('<xml/>');
 $xml->addAttribute('version', '1.0'); 
 $xml->addAttribute('encoding', 'UTF-8');
+
+// Base start date off of Today if it's Sunday, or last Sunday if it has already passed.
+$stream_title = "Watermark Sunday";
 
 $datestring = date('D');
 if($datestring == 'Sun'){
@@ -15,13 +15,14 @@ else{
 	$datestring	= strtotime('last Sunday');	
 }
 //$datestring		= strtotime('last Sunday');
-$week_start 	= date('Y-m-d', $datestring); 										// Start date is in YYYY-MM-DD format, this date will be relative to server time.
-$rebroadcast 	= array ("00:00","03:59","07:59","11:59","15:59","19:59"); 			// 24 Hour Schedule of Rebroadcast Times, Relative to Account Timezone
-$assets 		= array ("filename.mp4");											// Files to be Assoicated with Events, Does Not Check if they Actually Exist on Service
-$streamname 	= "yourstreamname";													// Stream to push simulated live events to.
+$week_start 	= date('Y-m-d', $datestring); 																	// Start date is in YYYY-MM-DD format, this date will be relative to server time.
+$rebroadcast 	= array ("00:00","03:58","07:57","11:58","15:58","19:58"); 										// 24 Hour Schedule of Rebroadcast Times, Relative to Account Timezone
+$assets 		= array ("20170910_0855_2200_0000.mp4");														// Files to be Assoicated with Events, Does Not Check if they Actually Exist on Service, previously this could've been an array of inputs, as of sometime in 2016 they moved to transcoding the input files in Amazon Elastic Transcoder - now we only feed a single asset.
+$streamname 	= "yourstreamname";																				// Stream to push simulated live events to.
 
 $simlive_day = array(
-    "sunday"	=> array ("12:59", "14:59","16:59"), // Sunday exception to normal schedule.
+	//"sunday"	=> $rebroadcast,					// Use the regular broadcast schedule, uncomment this line if you'd like this day to be generated.
+    "sunday"	=> array ("12:58","14:58","16:58"), // Make an exception to the schedule, override with a custom arrahy of values. These values are for a 24 clock, values 0-23.
     "monday"	=> $rebroadcast,
    	"tuesday"	=> $rebroadcast,
     "wednesday" => $rebroadcast,
@@ -48,6 +49,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>'."\r\n"; ?>
 							foreach ($assets as $files => $file) {						// Iterate through the array of files and create an file tag for each.
 							echo "				";
 							echo "<file>$file</file>\r\n";
+								//$file = $event->addChild('file', $file);
 							}
 						echo "			";
 						echo "</event>\r\n";
